d1d01932896ce170a65a8a41c427166b8cad54e7
Zeyad Sharo <33178745+zeyadsharo@users.noreply.github.com>
2022-03-15 15:28:49 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# Project Title
+
+A brief description of what this project does and who it's for
+
+
+## Demo
+[novel](https://novel-telecom.netlify.app/)
---
+++ tailwind.config.js
@@ -0,0 +1,67 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  mode: 'jit',
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    // classes that are generated dynamically, e.g. `rounded-${size}` and must
+    // be kept
+    safeList: [],
+    content: [
+      './index.html',
+      './src/**/*.{vue,js,ts}',
+      // etc.
+    ],
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+        typography: ({ theme }) => ({
+            pink: {
+                css: {
+                    '--tw-prose-body': theme('colors.pink[800]'),
+                    '--tw-prose-headings': theme('colors.pink[900]'),
+                    '--tw-prose-lead': theme('colors.pink[700]'),
+                    '--tw-prose-links': theme('colors.pink[900]'),
+                    '--tw-prose-bold': theme('colors.pink[900]'),
+                    '--tw-prose-counters': theme('colors.pink[600]'),
+                    '--tw-prose-bullets': theme('colors.pink[400]'),
+                    '--tw-prose-hr': theme('colors.pink[300]'),
+                    '--tw-prose-quotes': theme('colors.pink[900]'),
+                    '--tw-prose-quote-borders': theme('colors.pink[300]'),
+                    '--tw-prose-captions': theme('colors.pink[700]'),
+                    '--tw-prose-code': theme('colors.pink[900]'),
+                    '--tw-prose-pre-code': theme('colors.pink[100]'),
+                    '--tw-prose-pre-bg': theme('colors.pink[900]'),
+                    '--tw-prose-th-borders': theme('colors.pink[300]'),
+                    '--tw-prose-td-borders': theme('colors.pink[200]'),
+                    '--tw-prose-invert-body': theme('colors.pink[200]'),
+                    '--tw-prose-invert-headings': theme('colors.white'),
+                    '--tw-prose-invert-lead': theme('colors.pink[300]'),
+                    '--tw-prose-invert-links': theme('colors.white'),
+                    '--tw-prose-invert-bold': theme('colors.white'),
+                    '--tw-prose-invert-counters': theme('colors.pink[400]'),
+                    '--tw-prose-invert-bullets': theme('colors.pink[600]'),
+                    '--tw-prose-invert-hr': theme('colors.pink[700]'),
+                    '--tw-prose-invert-quotes': theme('colors.pink[100]'),
+                    '--tw-prose-invert-quote-borders': theme('colors.pink[700]'),
+                    '--tw-prose-invert-captions': theme('colors.pink[400]'),
+                    '--tw-prose-invert-code': theme('colors.white'),
+                    '--tw-prose-invert-pre-code': theme('colors.pink[300]'),
+                    '--tw-prose-invert-pre-bg': 'rgb(0 0 0 / 50%)',
+                    '--tw-prose-invert-th-borders': theme('colors.pink[600]'),
+                    '--tw-prose-invert-td-borders': theme('colors.pink[700]'),
+                },
+            },
+        }),
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ]
+}
