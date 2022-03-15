e9b05fa151281db2fe127b182eafaf5af4ab1fce
Zeyad Sharo <33178745+zeyadsharo@users.noreply.github.com>
2022-03-15 23:42:59 +0300

    Update viewProduct.vue

--- src/components/viewProduct.vue
+++ src/components/viewProduct.vue
@@ -75,10 +75,6 @@
                           </div>
                         </RadioGroup>
                       </div>
-                      <button type="submit"
-                              class="mt-6 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        Add to bag
-                      </button>
                     </form>
                   </section>
                 </div>
